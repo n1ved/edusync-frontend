ac921ae8ea4e76a62d0cf68342bddbfa46ee520b
Nived R S <nivedrsalini@gmail.com>
2024-10-24 16:59:04 +0530

    admin/reposition button

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link"
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 import {
+    EditIcon,
+    Paperclip, PersonStanding,
     PlusCircle,
 } from "lucide-react"
 
@@ -105,159 +107,13 @@ export default function Dashboard() {
     return (
         <TooltipProvider>
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
-            {/*<aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">*/}
-            {/*    <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">*/}
-            {/*        <Link*/}
-            {/*            href="#"*/}
-            {/*            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"*/}
-            {/*        >*/}
-            {/*            <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />*/}
-            {/*            <span className="sr-only">EduSync</span>*/}
-            {/*        </Link>*/}
-            {/*        <Tooltip>*/}
-            {/*            <TooltipTrigger asChild>*/}
-            {/*                <Link*/}
-            {/*                    href="#"*/}
-            {/*                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"*/}
-            {/*                >*/}
-            {/*                    <Home className="h-5 w-5" />*/}
-            {/*                    <span className="sr-only">Dashboard</span>*/}
-            {/*                </Link>*/}
-            {/*            </TooltipTrigger>*/}
-            {/*            <TooltipContent side="right">Dashboard</TooltipContent>*/}
-            {/*        </Tooltip>*/}
-            {/*        <Tooltip>*/}
-            {/*            <TooltipTrigger asChild>*/}
-            {/*                <Link*/}
-            {/*                    href="#"*/}
-            {/*                    className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"*/}
-            {/*                >*/}
-            {/*                    <ShoppingCart className="h-5 w-5" />*/}
-            {/*                    <span className="sr-only">Orders</span>*/}
-            {/*                </Link>*/}
-            {/*            </TooltipTrigger>*/}
-            {/*            <TooltipContent side="right">Orders</TooltipContent>*/}
-            {/*        </Tooltip>*/}
-            {/*        <Tooltip>*/}
-            {/*            <TooltipTrigger asChild>*/}
-            {/*                <Link*/}
-            {/*                    href="#"*/}
-            {/*                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"*/}
-            {/*                >*/}
-            {/*                    <Package className="h-5 w-5" />*/}
-            {/*                    <span className="sr-only">Products</span>*/}
-            {/*                </Link>*/}
-            {/*            </TooltipTrigger>*/}
-            {/*            <TooltipContent side="right">Products</TooltipContent>*/}
-            {/*        </Tooltip>*/}
-            {/*        <Tooltip>*/}
-            {/*            <TooltipTrigger asChild>*/}
-            {/*                <Link*/}
-            {/*                    href="#"*/}
-            {/*                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"*/}
-            {/*                >*/}
-            {/*                    <Users2 className="h-5 w-5" />*/}
-            {/*                    <span className="sr-only">Customers</span>*/}
-            {/*                </Link>*/}
-            {/*            </TooltipTrigger>*/}
-            {/*            <TooltipContent side="right">Customers</TooltipContent>*/}
-            {/*        </Tooltip>*/}
-            {/*        <Tooltip>*/}
-            {/*            <TooltipTrigger asChild>*/}
-            {/*                <Link*/}
-            {/*                    href="#"*/}
-            {/*                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"*/}
-            {/*                >*/}
-            {/*                    <LineChart className="h-5 w-5" />*/}
-            {/*                    <span className="sr-only">Analytics</span>*/}
-            {/*                </Link>*/}
-            {/*            </TooltipTrigger>*/}
-            {/*            <TooltipContent side="right">Analytics</TooltipContent>*/}
-            {/*        </Tooltip>*/}
-            {/*    </nav>*/}
-            {/*    <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">*/}
-            {/*        <Tooltip>*/}
-            {/*            <TooltipTrigger asChild>*/}
-            {/*                <Link*/}
-            {/*                    href="#"*/}
-            {/*                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"*/}
-            {/*                >*/}
-            {/*                    <Settings className="h-5 w-5" />*/}
-            {/*                    <span className="sr-only">Settings</span>*/}
-            {/*                </Link>*/}
-            {/*            </TooltipTrigger>*/}
-            {/*            <TooltipContent side="right">Settings</TooltipContent>*/}
-            {/*        </Tooltip>*/}
-            {/*    </nav>*/}
-            {/*</aside>*/}
+
             <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
                 <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-                    {/*<Sheet>*/}
-                    {/*    <SheetTrigger asChild>*/}
-                    {/*        <Button size="icon" variant="outline" className="sm:hidden">*/}
-                    {/*            <PanelLeft className="h-5 w-5" />*/}
-                    {/*            <span className="sr-only">Toggle Menu</span>*/}
-                    {/*        </Button>*/}
-                    {/*    </SheetTrigger>*/}
-                    {/*    <SheetContent side="left" className="sm:max-w-xs">*/}
-                    {/*        <nav className="grid gap-6 text-lg font-medium">*/}
-                    {/*            <Link*/}
-                    {/*                href="#"*/}
-                    {/*                className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"*/}
-                    {/*            >*/}
-                    {/*                <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />*/}
-                    {/*                <span className="sr-only">Acme Inc</span>*/}
-                    {/*            </Link>*/}
-                    {/*            <Link*/}
-                    {/*                href="#"*/}
-                    {/*                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"*/}
-                    {/*            >*/}
-                    {/*                <Home className="h-5 w-5" />*/}
-                    {/*                Dashboard*/}
-                    {/*            </Link>*/}
-                    {/*            <Link*/}
-                    {/*                href="#"*/}
-                    {/*                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"*/}
-                    {/*            >*/}
-                    {/*                <ShoppingCart className="h-5 w-5" />*/}
-                    {/*                Orders*/}
-                    {/*            </Link>*/}
-                    {/*            <Link*/}
-                    {/*                href="#"*/}
-                    {/*                className="flex items-center gap-4 px-2.5 text-foreground"*/}
-                    {/*            >*/}
-                    {/*                <Package className="h-5 w-5" />*/}
-                    {/*                Products*/}
-                    {/*            </Link>*/}
-                    {/*            <Link*/}
-                    {/*                href="#"*/}
-                    {/*                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"*/}
-                    {/*            >*/}
-                    {/*                <Users2 className="h-5 w-5" />*/}
-                    {/*                Customers*/}
-                    {/*            </Link>*/}
-                    {/*            <Link*/}
-                    {/*                href="#"*/}
-                    {/*                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"*/}
-                    {/*            >*/}
-                    {/*                <LineChart className="h-5 w-5" />*/}
-                    {/*                Settings*/}
-                    {/*            </Link>*/}
-                    {/*        </nav>*/}
-                    {/*    </SheetContent>*/}
-                    {/*</Sheet>*/}
 
                     <div className="relative ml-auto flex-1 md:grow-0">
                         <div className="ml-auto flex items-center gap-2">
-                            <a href={"/admin/dashboard/add"}>
 
-                                <Button size="sm" className="h-8 gap-1">
-                                    <PlusCircle className="h-3.5 w-3.5"/>
-                                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                                        Add Staff
-                                    </span>
-                                </Button>
-                            </a>
                         </div>
                     </div>
                     <DropdownMenu>
@@ -283,8 +139,19 @@ export default function Dashboard() {
                 </header>
                 <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
                     <Tabs defaultValue="all">
+                        <div className="ml-auto flex items-center gap-2 w-[92%] justify-center m-2 sm:w-fit">
+                            <a href={"/admin/dashboard/add"}>
+
+                                <Button size="sm" className="h-8 gap-1">
+                                    <PlusCircle className="h-3.5 w-3.5"/>
+                                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                        Add Staff
+                                    </span>
+                                </Button>
+                            </a>
+                        </div>
                         <div className="flex items-center">
-                           
+
                         </div>
                         <TabsContent value="all">
                             <Card x-chunk="dashboard-06-chunk-0">
@@ -318,11 +185,11 @@ export default function Dashboard() {
                                             {
                                                 staffData.map((staff, index) => (
                                                     <TableRowDynamic
-                                                        name= {staff.name}
-                                                        chargeOf= {staff.chargeOf}
-                                                        courses= {staff.courses}
-                                                        classes= {staff.classes}
-                                                        createdAt= {staff.createdAt}
+                                                        name={staff.name}
+                                                        chargeOf={staff.chargeOf}
+                                                        courses={staff.courses}
+                                                        classes={staff.classes}
+                                                        createdAt={staff.createdAt}
                                                         key={index}
                                                     />
                                                 ))
@@ -342,6 +209,6 @@ export default function Dashboard() {
                 </main>
             </div>
         </div>
-    </TooltipProvider>
+        </TooltipProvider>
     )
 }
