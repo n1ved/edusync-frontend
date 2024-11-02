5cfbb4c17fa6cc0dab5308bf5dc46e331dd6d258
Nived R S <nivedrsalini@gmail.com>
2024-11-02 23:17:05 +0530

    CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+//needs cross-origin resource sharing (CORS) enabled
+const nextConfig = {
+    async headers() {
+        return [
+        {
+            source: '/:path*',
+            headers: [
+            {
+                key: 'Access-Control-Allow-Origin',
+                value: '*',
+            },
+            {
+                key: 'Access-Control-Allow-Methods',
+                value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            },
+            {
+                key: 'Access-Control-Allow-Headers',
+                value: 'X-Requested-With, Content-Type, Accept',
+            },
+            ],
+        },
+        ];
+    },
+};
 
 export default nextConfig;
