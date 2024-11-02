c05f83589dc8ef0c0b6edbcbe8a5bef6da65ca3b
Nived R S <nivedrsalini@gmail.com>
2024-11-02 19:43:06 +0530

    clean login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image"
-
+//Import space grotesk font from google fonts nextjs
+import {Space_Grotesk} from "next/font/google";
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {TabsList, TabsTrigger , Tabs , TabsContent} from "@/components/ui/tabs";
-
+import {Space} from "lucide-react";
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export default function Login() {
     return (
@@ -12,7 +14,7 @@ export default function Login() {
             <div className="flex items-center justify-center py-12">
                 <div className="mx-auto grid w-[350px] gap-6">
                     <div className="grid gap-2 text-center">
-                        <h1 className="text-3xl font-bold">Login to EduSync</h1>
+                        <h1 className="text-3xl font-bold"></h1>
                     </div>
                     <Tabs defaultValue={"student"}>
                         <div className={"w-full flex justify-center"}>
@@ -57,16 +59,15 @@ export default function Login() {
                     </Tabs>
                 </div>
             </div>
-            <div className="hidden bg-muted lg:block">
-                <Image
-                    src="/login.png"
-                    alt="Image"
-                    width="1920"
-                    height="1080"
-
-                    className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
+            <div className="h-full lg:block flex flex-row bg-primary justify-center">
+                <div className={"h-[100vh] w-[50vw] bg-primary text-secondary text-8xl m-0 p-5 font-extrabold flex flex-col justify-center"}>
+                    <div className={spaceGrotesk.className}>
+                        <h2 className={"font-outline-2 text-primary"}>Welcome</h2>
+                        <h3 className={"font-outline-2 text-primary"}>to</h3>
+                        <h2 className={"text-9xl"}>Edusync</h2>
+                    </div>
+                </div>
             </div>
         </div>
-)
+    )
 }
